fa266ee97f962c0c835d06307c4ce470a751e85a
muhammadisa <msawjk@gmail.com>
2020-10-15 03:58:42 +0700

    Initial release

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/muhammadisa/gormdbcon
+
+go 1.14
+
+require (
+	github.com/denisenkom/go-mssqldb v0.0.0-20200910202707-1e08a3fab204 // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
+)
---
+++ gormdbcon.go
@@ -0,0 +1,75 @@
+package gormdbcon
+
+import (
+	"fmt"
+	"strings"
+
+	_ "github.com/jinzhu/gorm/dialects/mssql"    // MSSql Driver
+	_ "github.com/jinzhu/gorm/dialects/mysql"    // MySql Driver
+	_ "github.com/jinzhu/gorm/dialects/postgres" // Postgres Driver
+
+	"github.com/jinzhu/gorm"
+)
+
+// IDatabase interface
+type IDatabase interface {
+	Connect() (*gorm.DB, error)
+}
+
+// DBCredential struct
+type DBCredential struct {
+	DBDriver     string
+	DBHost       string
+	DBPort       string
+	DBUser       string
+	DBPassword   string
+	DBName       string
+	DBSqlitePath string
+}
+
+// Connect connect to selected database dialect
+func (dbCredential DBCredential) Connect() (*gorm.DB, error) {
+	var db *gorm.DB
+	var err error
+
+	drivers := []string{
+		fmt.Sprintf("mysql~%s", fmt.Sprintf(
+			"%s:%s@(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+			dbCredential.DBUser,
+			dbCredential.DBPassword,
+			dbCredential.DBHost,
+			dbCredential.DBPort,
+			dbCredential.DBName,
+		)),
+		fmt.Sprintf("postgres~%s", fmt.Sprintf(
+			"host=%s port=%s user=%s sslmode=disable dbname=%s password=%s",
+			dbCredential.DBHost,
+			dbCredential.DBPort,
+			dbCredential.DBUser,
+			dbCredential.DBName,
+			dbCredential.DBPassword,
+		)),
+		fmt.Sprintf("mssql~%s", fmt.Sprintf(
+			"sqlserver://%s:%s@%s:%s?database=%s",
+			dbCredential.DBUser,
+			dbCredential.DBPassword,
+			dbCredential.DBHost,
+			dbCredential.DBPort,
+			dbCredential.DBName,
+		)),
+	}
+
+	for index := range drivers {
+		drv := strings.Split(drivers[index], "~")
+		if dbCredential.DBDriver == drv[0] {
+			db, err = gorm.Open(dbCredential.DBDriver, drv[1])
+			if err != nil {
+				return &gorm.DB{}, err
+			}
+			break
+		}
+	}
+
+	fmt.Println("Database Connected")
+	return db, nil
+}
